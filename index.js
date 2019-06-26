9efd846563b524aa485c38e9a7ff8ebd5fceeee1
George Allison <49288165+joji-harada@users.noreply.github.com>
2019-06-25 21:32:13 -0700

    First commit - homepage / about page switch.
    GitHub wouldn't let me upload the modules file because there were too many files.

---
+++ index.js
@@ -0,0 +1,26 @@
+const http = require("http"); 
+
+http.createServer((req,res) => {
+  const path = req.url.toLowerCase();
+  const fs = require("fs");
+  switch(path) {
+    case '/':
+       fs.readFile("public/home.html", (err, data) => {
+         if (err) return console.error(err);
+         res.writeHead(200, {'Content-Type': 'text/html'});
+         res.end(data.toString());
+        });
+      break;
+    case '/about':
+       fs.readFile("public/about.html", (err, data) => {
+         if (err) return console.error(err);
+         res.writeHead(200, {'Content-Type': 'text/html'});
+         res.end(data.toString());
+        });
+      break;
+    default:
+      res.writeHead(404, {'Content-Type': 'text/plain'});
+      res.end('Not found');
+      break;
+    }
+}).listen(process.env.PORT || 3000);
