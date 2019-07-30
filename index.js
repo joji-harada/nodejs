97708462cda43106b33090bc180d4bceb83c66ac
George Albert Allison <jojiharada@Georges-MacBook-Pro.local>
2019-07-30 15:29:24 -0700

    REST API's

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict'
-// const computers = require("./lib/data");
+// const computerdata = require("./lib/data");
 const computers = require('./models/computer');
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -8,6 +8,7 @@ const app = express();
 app.set('port', process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public')); // set location for static files
 app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
+app.use('/api', require('cors')()); //set Access-control-allow-origin header for api route
 
 const handlebars = require("express-handlebars");
 app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
@@ -18,21 +19,21 @@ app.set("view engine", ".html");
 the curly brackets accesses the {{}} in view/home.html file
 and inserts the key-value pair into the {{}} statement*/
 app.get('/', (req, res) => {
-    computers.find({}, {'_id': false}, (err, items) => {
+    computers.find({}, {'_id': false}, (err, items, next) => {
         if (err) return next(err);
         res.render('home', {computers:items});
     });
 });
     
 // handle form submission response
-app.post('/detail', (req,res) => {
+app.post('/detail', (req,res,next) => {
     computers.findOne({'title':req.body.title}, {'_id':false}, (err, item) => {
         if (err) return next(err);
         res.render('detail',{ computer: item });
     })
 });
 
-app.get('/detail', (req,res) => {
+app.get('/detail', (req,res,next) => {
     computers.findOne({'title':req.query.title}, {'_id':false}, (err, item) => {
         if (err) return next(err);
         res.render('detail',{ computer: item });
@@ -47,7 +48,7 @@ app.get('/about', (req, res) => {
 
 //delete item
 app.get('/delete', (req,res) => {
-    computers.deleteOne({'title':req.query.title}, (err, item) => {
+    computers.deleteOne({'title':req.query.title}, (err, next) => {
         if (err) return next(err);
         computers.countDocuments((err, result) => {
             res.render('delete', {
@@ -74,6 +75,41 @@ app.post('/add', (req,res) => {
         })
     })
 });
+
+//api routes
+//api getAll
+app.get('/api/detail', (req,res,next) => {
+    computers.find({},{"_id":false}, (err, items) => {
+        if (err) return next(err);
+        res.json(items);
+    })
+});
+
+//api getOne
+app.get('/api/detail/:title', (req,res,next) => {
+    computers.findOne({title:req.params.title},{"_id":false},(err, item) => {
+        if (err) return next(err);
+        res.json(item);
+    })
+});
+
+//api delete
+app.get('/api/delete/:title', (req,res,next) => {
+    computers.deleteOne({title:req.params.title}, (err, item) => {
+        if (err) return next(err);
+        res.json(item);
+    })
+});
+
+
+//api add
+app.post('/api/add/', (req,res,next) => {
+    computers.update({'title':req.body.title}, req.body, {upsert:true}, (err, result) => {
+        console.log(result);
+        res.json({/*updated: result.nModified,*/ title: req.body.title});
+    })
+});
+
 
 // define 404 handler
 app.use( (req,res) => {
